69a04064446c8707eda0e1ef13330ea4fd093514
LAPTOP-5RU3UB56\Berke <berken_93@hotmail.com>
2024-06-04 17:39:46 +0200

    Started my portfolio page. Almoust done too. Only need to make a projects section and contact me. After that its just scalability to different screensizes left and some js.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.getElementById("burgerOpen").addEventListener("click", function () {
+    let menu = document.getElementById("burgerMenu"); // Changed to target #burgerMenu
+    let burgerOpenIcon = document.getElementById("burgerOpenIcon");
+    let burgerCloseIcon = document.getElementById("burgerCloseIcon");
+    if (menu.style.display === "none" || menu.style.display === "") { 
+        menu.style.display = "flex";
+        burgerOpenIcon.style.display = "none";
+        burgerCloseIcon.style.display = "block";
+    } else {
+        menu.style.display = "none";
+        burgerOpenIcon.style.display = "block";
+        burgerCloseIcon.style.display = "none";
+    }
+});
